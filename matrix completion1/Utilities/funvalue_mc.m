aad1bdd8f7eaca3e6b0d3b62cd3c4a140b596ce0
lihuanML <67913302+lihuanML@users.noreply.github.com>
2023-04-25 22:17:12 +0800

    Delete matrix completion1 directory

--- matrix completion1/Utilities/funvalue_mc.m
+++
@@ -1,6 +0,0 @@
-function [obj] = funvalue_mc(data_train,I_train,J_train,len,U,V)
-
-M = partXY(U',V',I_train,J_train,len);               % calculate Proj(U*V') and store it in the vector M, where Proj means the projection onto the index set of the observed samples
-mat = M' - data_train;                               % calculate Proj(U*V'-X*) and store it in the vector mat; X* is the observed sample matrix
-
-obj = ( norm(mat)^2 + norm((U'*U - V'*V),'fro')^2 )/2/len;         % calculate the function value at (U,V)
